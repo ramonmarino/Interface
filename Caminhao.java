a6c6acf29606756caffe1f2f074e3c2169950f76
Ramon Marino <ramonmarino12@gmail.com>
2023-04-25 19:34:37 -0300

    Nova Versão

--- Caminhao.java
+++ Caminhao.java
@@ -1,31 +1,28 @@
 package Montadora;
 
-import java.util.ArrayList;
-
-public class Carro implements CarroFuncoes {
+public class Caminhao implements CarroFuncoes {
 
     private String cor;
+
     private String modelo;
 
     private String tipo;
 
-    public Carro(String cor, String modelo, String tipo) {
+    public Caminhao(String cor, String modelo, String tipo) {
         this.cor = cor;
         this.modelo = modelo;
         this.tipo = tipo;
     }
 
-    public Carro() {
+    public Caminhao(){
 
     }
-
 
     public String getCor() {
         return cor;
     }
 
     public void setCor(String cor) {
-
         this.cor = cor;
     }
 
@@ -34,42 +31,39 @@ public class Carro implements CarroFuncoes {
     }
 
     public void setModelo(String modelo) {
-
         this.modelo = modelo;
     }
 
     public String getTipo() {
-
         return tipo;
     }
 
     public void setTipo(String tipo) {
-
         this.tipo = tipo;
     }
 
     @Override
     public String frente() {
-        return "Carro " + cor + " foi para frente";
+         return "Caminhão " + cor + " foi para frente";
     }
 
     @Override
     public String atras() {
-        return "Carro " + cor + " foi para tras";
+         return "Caminhão " + cor + " foi para tras";
     }
 
     @Override
     public String esquerda() {
-        return "Carro " + cor + " foi para esquerda";
+         return "Caminhão " + cor + " foi para esquerda";
     }
 
     @Override
     public String direita() {
-        return "Carro " + cor + " foi para direita";
+         return "Caminhão " + cor + " foi para direita";
     }
 
     @Override
     public String toString() {
-         return "Carro " + cor + " "+ modelo + " motor " + tipo;
+        return "Caminhão " + cor + " "+ modelo + " motor " + tipo;
     }
 }
--- Pista.java
+++ Pista.java
@@ -1,44 +1,124 @@
 package Montadora;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+/*
+            CHECK LIST
+
+Preciso fazer um software que faça....
+
+         Passo 1- Adicionar mais uma opção de automóvel(caminhão) para o usuário.
+
+         Passo 2- O caminhão ter as mesmas características dos carros.
+
+         Passe 3- nesse passo, vou adicionar uma quantidade de automóveis de acordo com que o
+         usuário desejar.
+
+         Passo 4- Fazer a escolha de qual automóvel o usuário quer usar, carro ou caminhão.
+
+         Passo 5- Fazer a personalização de acordo com atributos disponíveis, na cor,modelo e tipo que o
+         usuário desejar.
+
+         Passo 6- Adicionar os automóveis fabricados no "catálago" para o usuário
+
+         Passo 7- Depois que o usuário escolher o automóvel desejado oferecer para ele uma direção  na qual ele
+         deseja ir com o automóvel selecionado.
+
+         Passo 8- Oferecer para o usuário um critério para ele encerrar o programa ou continuar com outra outra opção
+         Nota: O programa não deve ser encerrado até o usuário desejar.
+
+         Passo 9- programa encerrado.
+
+
+
+
+ */
 public class Pista {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Carro carro = new Carro("azul","volkswagen","elétrico");
-        boolean aux = true;
-        while(aux){
-            System.out.println("ESCOLHA A OPÇÃO DESEJADA: (1/2/3/4): ");
-            int resp = input.nextInt();
-            switch (resp){
-                case 1:
-                    System.out.println(carro.frente());
-                    break;
-                case 2:
-                    System.out.println(carro.atras());
-                    break;
 
-                case 3:
-                    System.out.println(carro.esquerda());
-                    break;
+        List<CarroFuncoes> carrosCadastros = new ArrayList<>();
 
-                case 4:
-                    System.out.println(carro.direita());
-                    break;
-                default:
-                    aux = false;
-                    System.out.println("opção de inválida!");
-                    break;
+        System.out.print("DEFINA A QUANTIDADE DE AUTOMÓVEIS A SEREM FABRICADOS: ");
+        int resposta = input.nextInt();
+
+
+        for (int i = 1; i <= resposta; i++) {
+            System.out.println("Deseja fabricar carro ou caminhão?\n1 Carro\n2 Caminhão");
+            int escolha = input.nextInt();
+            System.out.print("Qual a cor ? ");
+            String corCarro = input.next();
+            System.out.print("Qual o modelo? ");
+            String modeloCarro = input.next();
+            System.out.print("Qual o combustivel? ");
+            String tipoCarro = input.next();
+
+            if (escolha == 1) {
+                carrosCadastros.add(new Carro(corCarro, modeloCarro, tipoCarro));
+
+            } else {
+                carrosCadastros.add(new Caminhao(corCarro, modeloCarro, tipoCarro));
+
             }
 
         }
 
+            boolean isTrue = true;
+            while (isTrue) {
+
+               for(CarroFuncoes carro : carrosCadastros) {
+                    System.out.println(carrosCadastros.indexOf(carro) + 1 + " " + carro);
+                }
+
+                System.out.println("Para sair digite uma opção fora do intervalo!");
+
+                System.out.print("Qual veiculo deseja usar: ");
+                int opcao = input.nextInt();
+                if(opcao > carrosCadastros.size() || opcao <= 0 ) {
+                    System.out.println("Saindo....");
+                    break;
+                }
+
+                CarroFuncoes automovelSelecionado = carrosCadastros.get(opcao -1) ;
+
+                System.out.println("Selecione a direção:\n1-Frente\n2-Tras\n3-Esquerda\n4-Direita");
+
+                int direcao = input.nextInt();
+                switch (direcao) {
+                    case 1:
+                        System.out.println(automovelSelecionado.frente());
+                        break;
+
+                    case 2:
+                        System.out.println(automovelSelecionado.atras());
+                        break;
+
+                    case 3:
+                        System.out.println(automovelSelecionado.esquerda());
+                        break;
+
+                    case 4:
+                        System.out.println(automovelSelecionado.direita());
+                        break;
+
+                    default:
+                        isTrue = false;
+                        System.out.println("opção de inválida!");
+                        break;
+
+                }
 
 
 
+            }
+
+            input.close();
+        }
 
 
-        input.close();
     }
 
 
@@ -48,5 +128,7 @@ public class Pista {
 
 
 
-}
+
+
+
 
